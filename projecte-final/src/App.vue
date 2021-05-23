8a896f48c09ec2d3530630a43eab3c8d14b0038f
raffaellapagano <raffaellapagano@yahoo.it>
2021-05-23 20:10:55 +0200

    Navbar

--- projecte-final/src/App.vue
+++ projecte-final/src/App.vue
@@ -1,8 +1,29 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">SPA</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarSupportedContent">
+              <router-link class="nav-link" to="/">Home</router-link>           
+              <router-link class="nav-link" to="/users/general">Users</router-link>
+              <router-link class="nav-link" to="/picture">Picture</router-link>
+        </div>
+
+        <form class="form-inline my-2 d-flex justify-content-center">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-light my-2 my-sm-0" type="submit">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+          </svg>
+          </button>
+        </form>        
+      </nav>
+
     </div>
     <router-view/>
   </div>
@@ -22,7 +43,7 @@
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #f9fbfc;
 
     &.router-link-exact-active {
       color: #42b983;
